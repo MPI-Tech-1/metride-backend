3f81da23bdd22fbc86fe330938fa73ea6379c6bb
Dauda Pona <daudapona2@gmail.com>
2026-04-16 08:22:08 +0100

    feat: added support for worker

--- .adonisjs/client/manifest.d.ts
+++ .adonisjs/client/manifest.d.ts
@@ -5,5 +5,8 @@
 
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/auth.ts" />
+/// <reference path="../../config/cache.ts" />
 /// <reference path="../../config/hash.ts" />
 /// <reference path="../../config/logger.ts" />
+/// <reference path="../../config/queue.ts" />
+/// <reference path="../../config/redis.ts" />
--- adonisrc.ts
+++ adonisrc.ts
@@ -28,6 +28,8 @@ export default defineConfig({
     () => import('@adonisjs/core/commands'),
     () => import('@adonisjs/lucid/commands'),
     () => import('@adonisjs/session/commands'),
+    () => import('@adonisjs/queue/commands'),
+    () => import('@adonisjs/cache/commands')
   ],
 
   /*
@@ -53,6 +55,9 @@ export default defineConfig({
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/auth/auth_provider'),
     () => import('#providers/api_provider'),
+    () => import('@adonisjs/queue/queue_provider'),
+    () => import('@adonisjs/cache/cache_provider'),
+    () => import('@adonisjs/redis/redis_provider')
   ],
 
   /*
@@ -67,6 +72,10 @@ export default defineConfig({
     () => import('#start/routes'),
     () => import('#start/kernel'),
     () => import('#start/validator'),
+    {
+      file: () => import('#start/scheduler'),
+      environment: ['web'],
+    }
   ],
 
   /*
