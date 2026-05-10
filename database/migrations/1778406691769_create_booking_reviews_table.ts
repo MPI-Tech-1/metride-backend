da1e37340dbe4e28dc0d8050010b5abe9a417df1
Dauda Pona <daudapona2@gmail.com>
2026-05-10 11:33:07 +0100

    chore: code cleanup

--- database/migrations/1778406691769_create_booking_reviews_table.ts
+++ database/migrations/1778406691769_create_booking_reviews_table.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('identifier').index()
       table.integer('customer_id').index()
       table.integer('driver_id').index()
       table.integer('booking_id').index()
